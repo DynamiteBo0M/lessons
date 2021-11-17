9d3747dda14eeecf32ed9d502fa2d69cfbef299d
DynamiteBo0M <timad@ya.ru>
2021-11-17 21:36:10 +0300

    Add solution-01 for lesson-05

---
+++ lesson-05/solution-01.py
@@ -0,0 +1,12 @@
+"""
+Написать функцию, которая получает на вход имя и выводит строку вида: f"Hello, {name}". Создать список из 5 имен.
+Вызвать функцию для каждого элемента списка в цикле.
+"""
+
+def my_format(name):
+    result_string = f"hello, {name}"
+    return result_string
+
+my_list = ["Olga", "Alex", "Dima", "Roma", "Denis"]
+for name in my_list:
+    print(my_format(name))
